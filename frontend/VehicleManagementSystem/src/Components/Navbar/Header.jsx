f84573ad30e4d3b5ddf10a6efb32cc661de8ffe9
tushardhamak <tushardhamak657@gmail.com>
2025-07-23 20:34:12 +0530

    added homepage common for all

--- frontend/VehicleManagementSystem/src/Components/Navbar/Header.jsx
+++ frontend/VehicleManagementSystem/src/Components/Navbar/Header.jsx
@@ -18,7 +18,7 @@ const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">Vehicle Rental Management System</Link>
+        <Link className="navbar-brand" to="/">RentARide</Link>
         <button
           className="navbar-toggler"
           type="button"
