190dcb2e393bb7a64c7b5c114cc054c0e84944e8
raj_yass <yashraj813203@gmail.com>
2025-07-25 01:25:00 +0530

    Added Vehicle details page

--- frontend/VehicleManagementSystem/src/App.jsx
+++ frontend/VehicleManagementSystem/src/App.jsx
@@ -1,3 +1,5 @@
+
+
 import { Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import HomePage from "./Pages/Home/HomePage";
@@ -9,6 +11,20 @@ import AddCompanyForm from "./Pages/Admin/AddCompanyForm";
 import AddVariantForm from "./Pages/Admin/AddVariantForm";
 import AddVehicleForm from "./Pages/Admin/VehicleComponent/AddVehicleForm";
 import ViewAllVehicles from "./Pages/Admin/VehicleComponent/ViewAllVehicles";
+import CustomerDashboard from './Pages/Customer/CustomerDashboard';
+import CarDetailsPage from './Pages/Customer/CarDetailsPage';
+import BookingDetailsPage from './Pages/Customer/BookingDetailsPage';
+import MyBookingsPage from './Pages/Customer/MyBookingsPage';
+import UpdateProfilePage from './Pages/Customer/UpdateProfilePage';
+import MyProfilePage from './Pages/Customer/MyProfilePage';
+import PaymentPage from './Pages/Customer/PaymentPage';
+import HelpSupport from "./Pages/Customer/HelpSupport";
+import ViewAllCustomers from "./Pages/Admin/CustomerComponent/ViewAllCustomers";
+import ViewCustomerBooking from "./Pages/Admin/BookingComponent/ViewCustomerBooking";
+import UpdateVehicleForm from "./Pages/Admin/VehicleComponent/UpdateVehicleForm";
+
+
+
 function App() {
   return (
     <div>
@@ -23,6 +39,21 @@ function App() {
         <Route path="/admin/add-variant" element={<AddVariantForm />} />
         <Route path="/admin/add-vehicle" element={<AddVehicleForm />} />
         <Route path="/admin/vehicles" element={<ViewAllVehicles />} />
+        <Route path="/admin/view-customers" element={<ViewAllCustomers />} />
+        <Route path="/admin/bookings" element={<ViewCustomerBooking/>} />
+        <Route path="/admin/update-vehicle/:id" element={<UpdateVehicleForm />} />
+        <Route path="/admin/view-vehicle/:id" element={<VehicleDetails/>}/>
+
+        
+{/* ------------------------------------------------------------------------------------------ */}
+        <Route path="/customer/customer-dashboard" element={<CustomerDashboard />} />
+        <Route path="/customer/car-details/:id" element={<CarDetailsPage />} />
+        <Route path="/customer/booking-details/:id" element={<BookingDetailsPage />} />
+        <Route path="/customer/my-bookings" element={<MyBookingsPage />} />
+        <Route path="/customer/update-profile" element={<UpdateProfilePage />} />
+        <Route path="/customer/my-profile" element={<MyProfilePage />} />
+        <Route path="/customer/payment/:id" element={<PaymentPage />} />
+         <Route path="/customer/help-support" element={<HelpSupport />} />
         
       </Routes>
     </div>
