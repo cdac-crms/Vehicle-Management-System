472709760fcc98dc49104f1711c33a0f45d82808
shoyeb03 <sayyadshoyeb37@gmail.com>
2025-08-06 01:13:01 +0530

    Added bean for Cloudinary in Application.java

--- backend/VehicleManagementSystem/src/main/java/com/vms/Application.java
+++ backend/VehicleManagementSystem/src/main/java/com/vms/Application.java
@@ -1,11 +1,16 @@
 package com.vms;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.modelmapper.Conditions;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import com.cloudinary.Cloudinary;
 
 @SpringBootApplication // includes @Configuration
 public class Application {
@@ -38,6 +43,19 @@ public class Application {
 				.setPropertyCondition(Conditions.isNotNull());// use case - PUT
 		return mapper;
 
+	}
+	
+	
+	@Bean
+	public Cloudinary getCloudinary()
+	{
+		Map config = new HashMap();
+		config.put("cloud_name", "dklmsewsu");
+		config.put("api_key", "771414175948684");
+		config.put("api_secret", "AEPVNRsumqv9KvGhFEPT_Aq57D0");
+		config.put("secure", true);
+		
+		return new Cloudinary(config);
 	}
 
 }
